7832aaecec883dfd02fe706841d33c444b4ade3c
Shrinivas Kamath <shrinivas.kamath@gmail.com>
2023-03-25 02:43:36 +0530

    build failure logs

--- complete_build.sh
+++ complete_build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+set -xe
+
+export WLD=$HOME/wld
 meson subprojects download
 
-(cd subprojects/wayland-1.20.0 && meson setup build -Dlibraries=false -Dtests=false -Ddtd_validation=false --prefix=$HOME/wld/host_dep && ninja -C build install)
-export PKG_CONFIG_PATH=$HOME/wld/host_dep/lib/x86_64-linux-gnu/pkgconfig
 
-meson setup build --prefix=$HOME/wld --cross-file aarch64-linux-gnu.txt -Dwayland:documentation=false
+(cd subprojects/wayland-1.20.0 && meson setup build -Dlibraries=false -Dtests=false -Ddtd_validation=false --prefix=$WLD/host_dep && ninja -C build install)
+export PKG_CONFIG_PATH=$WLD/host_dep/lib/x86_64-linux-gnu/pkgconfig
+
+meson setup build --prefix=$WLD --cross-file aarch64-linux-gnu.txt -Dwayland:documentation=false
 ninja -C build && ninja -C build install
