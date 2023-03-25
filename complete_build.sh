b747ccf146bac737581165bfd7c70fbbe09b896a
Shrinivas Kamath <shrinivas.kamath@gmail.com>
2023-03-25 12:24:07 +0000

    wayland cross compile works

--- complete_build.sh
+++ complete_build.sh
@@ -9,5 +9,5 @@ meson subprojects download
 (cd subprojects/wayland-1.20.0 && meson setup build -Dlibraries=false -Dtests=false -Ddtd_validation=false --prefix=$WLD/host_dep && ninja -C build install)
 export PKG_CONFIG_PATH=$WLD/host_dep/lib/x86_64-linux-gnu/pkgconfig
 
-meson setup build --prefix=$WLD --cross-file aarch64-linux-gnu.txt -Dwayland:documentation=false
+meson setup build --prefix=$WLD --cross-file aarch64-linux-gnu.txt -Dwayland:documentation=false -Dlibffi:tests=false -Dbuild.pkg_config_path=$WLD/host_dep/lib/x86_64-linux-gnu/pkgconfig 
 ninja -C build && ninja -C build install
